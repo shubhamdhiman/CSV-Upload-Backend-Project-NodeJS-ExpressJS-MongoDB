27a7d9b6cdbc57dc97391f504eea4cffd3c8efaa
shubhamdhiman <shubhamdhiman00.sd@gmail.com>
2023-07-05 19:49:53 +0530

    file data rendering in table is done

--- controllers/csvController.js
+++ controllers/csvController.js
@@ -5,11 +5,8 @@ const csvParser = require("csv-parser")
 const asyncHandler = require("express-async-handler")
 const filePage = asyncHandler(
     async function(req,res){
-        // console.log(req.params)
-        // const csvFile = files.findOne({file:req.params.id})
-        // console.log(csvFile)
         let csvFile = await files.findOne({ file: req.params.id });
-        // console.log(csvFile);
+        console.log(csvFile);
         const results = [];
         const header = [];
         fs.createReadStream(csvFile.filePath) //seeting up the path for file upload
@@ -18,27 +15,18 @@ const filePage = asyncHandler(
                 headers.map((head) => {
                     header.push(head);
                 });
-                // console.log(header);
             })
             .on('data', (data) =>
                 results.push(data))
             .on('end', () => {
-                // console.log(results.length);
-                // console.log(results);
-                console.log(header)
-                // console.log(results)
                 res.render("filePage", {
                     title: "File Page", 
-                    fileName: csvFile.fileName,
+                    fileName: csvFile.fileOriginalName,
                     head: header,
                     data: results,
                     length: results.length
                 });
             });
-//         fs.createReadStream(file.path).pipe(csvParse()).on('data',function(data){
-//     console.log( "this is data...")
-//   })
-        // res.render("filePage")
     }
 )
 const uploadPage = asyncHandler(
